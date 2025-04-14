ad188015ae9c4bacc01cd59282df7861c8586dff
CarlGao4 <gaoweiqicarl1@163.com>
2025-04-14 17:52:03 +0800

    Add custom audio support

--- src/js/mscz-html.js
+++ src/js/mscz-html.js
@@ -64,6 +64,33 @@ let msczHtml = {
             msczHtml.ready();
         }
     },
+    /**
+     * Renders and displays the specified JSON data into an HTML element.
+     *
+     * @async
+     * @function displayMsczJson
+     * @param {string|HTMLElement} cssSelector - A CSS selector string to select the target HTML element, or the target HTMLElement itself.
+     * @param {string|Object} jsonFile - The URL string of the JSON file, or the JSON data object directly.
+     * @param {Object|null} config - Configuration object for customizing rendering behavior (optional).
+     * Available options:
+     * - `autoScroll`: A boolean indicating whether to enable auto-scrolling (default: true).
+     * - `audio`: An array, all items in the array should be objects with `src`, `name` and optional `measures` attribute. `measures` should be an array of objects with `id` and `time` attributes. `id` starts from 0 and `time` is in seconds.
+     * 
+     * @throws {Error} Throws an error if `cssSelector` is neither a string nor an HTMLElement.
+     * @throws {Error} Throws an error if the specified HTML element cannot be found.
+     * @throws {Error} Throws an error if `jsonFile` is neither a string nor an object.
+     * @throws {Error} Throws an error if the JSON data is missing required keys (e.g., `svgs`, `mposXML`, `metadata`, `midi`).
+     * 
+     * @example
+     * // Using a CSS selector to load JSON data
+     * msczHtml.displayMsczJson('#my-element', 'path/to/data.json', { autoScroll: true });
+     * 
+     * @example
+     * // Using an HTMLElement and directly passing JSON data
+     * const element = document.getElementById('my-element');
+     * const jsonData = { svgs: [...], mposXML: "...", metadata: {...}, midi: "..." };
+     * msczHtml.displayMsczJson(element, jsonData, { autoScroll: false });
+     */
     displayMsczJson: async (cssSelector, jsonFile, config) => {
         let currentId = load_idx++;
         let element;
@@ -111,7 +138,7 @@ let msczHtml = {
         html += `</select>`;
         html += `</div>`;
         html += `<div class="mscz-player-scroll">`;
-        html += `<input type="checkbox" checked>Auto Scroll</button>`;
+        html += `<input type="checkbox" ${((config && config.autoScroll !== undefined && config.autoScroll) || true) ? "checked" : ""}>Auto Scroll</button>`;
         html += `</div>`;
         html += `<div class="mscz-player-controls-time">${numberToTime(0)} / ${numberToTime(data.metadata.duration)}</div>`;
         html += `<div class="mscz-player-controls-seek">`;
@@ -161,12 +188,36 @@ let msczHtml = {
             times[0].push({ id: parseInt(measure.getAttribute("elid")), time: parseFloat(measure.getAttribute("position")) / 1000 });
             var measure_element = element.querySelector(`#mscz-measure-${currentId}-${measure.getAttribute("elid")}`);
             if (measure_element && !measure_element.hasAttribute("time")) {
-                measure_element.setAttribute("time", measure.getAttribute("position"));
+                measure_element.setAttribute("time", parseFloat(measure.getAttribute("position")) / 1000);
             }
         }
         times[0].sort((a, b) => a.time - b.time);
 
         let midi_player;
+        let audio_players = [];
+        if (config && config.audio) {
+            for (let i = 0; i < config.audio.length; i++) {
+                const audio = config.audio[i];
+                if (audio.src && audio.name) {
+                    let audio_player = new Audio();
+                    audio_player.crossOrigin = "*";
+                    audio_player.loop = true;
+                    // We don't need to set the src here to avoid preloading the audio
+                    if (audio.measures) {
+                        times.push([]);
+                        for (let measure of audio.measures) {
+                            times[i + 1].push({ id: measure.id, time: measure.time });
+                        }
+                    }
+                    else {
+                        times.push(times[0]);
+                    }
+                    times[i + 1].sort((a, b) => a.time - b.time);
+                    element.querySelector(".mscz-track-select").innerHTML += `<option value="${i + 1}">${audio.name}</option>`;
+                    audio_players.push(audio_player);
+                }
+            }
+        }
 
         const midiData = atob(data.midi);
         const midi_byteNumbers = new Array(midiData.length);
@@ -177,6 +228,7 @@ let msczHtml = {
         const midi_arrayBuffer = midi_byteArray.buffer;
         element.querySelector(".mscz-play-button").addEventListener("click", async () => {
             if (!midi_player) {
+                element.querySelector("select").value = "0";
                 let context = new AudioContext();
                 await context.audioWorklet.addModule(new URL(SpessaSynth_url_prefix + global_vars.WORKLET_URL_ABSOLUTE));
                 let synth = new global_vars.Synthetizer(context.destination, sf3);
@@ -184,16 +236,15 @@ let msczHtml = {
                     binary: midi_arrayBuffer,
                     altName: "midi",
                 }], synth);
+                audio_players.unshift(midi_player);
                 const slider = element.querySelector("input[type=range]");
                 const setCurrentTime = (time) => {
                     let track_id = parseInt(element.querySelector("select").value);
-                    if (track_id === 0) {
-                        element.querySelector(".mscz-player-controls-time").innerText = `${numberToTime(time)} / ${numberToTime(midi_player.duration)}`;
-                        // Only adjust the slider if it is not active
-                        if (!slider.matches(':active')) {
-                            slider.value = time * 10;
-                            slider.setAttribute("max", midi_player.duration * 10);
-                        }
+                    element.querySelector(".mscz-player-controls-time").innerText = `${numberToTime(time)} / ${numberToTime(audio_players[track_id].duration)}`;
+                    // Only adjust the slider if it is not active
+                    if (!slider.matches(':active')) {
+                        slider.value = time * 10;
+                        slider.setAttribute("max", audio_players[track_id].duration * 10);
                     }
                     let times_current = times[track_id];
                     let idx = bisect_right(times_current, time, 0, times_current.length, (x) => x.time) - 1;
@@ -215,35 +266,56 @@ let msczHtml = {
                 }
                 const adjustCurrentTime = (time) => {
                     let track_id = parseInt(element.querySelector("select").value);
-                    if (track_id === 0) {
-                        midi_player.currentTime = time;
-                    }
+                    audio_players[track_id].currentTime = time;
                 }
                 slider.onchange = () => {
                     adjustCurrentTime(slider.value / 10);
                 }
                 for (let i of element.querySelectorAll(".mscz-measure-overlay")) {
                     i.onclick = () => {
-                        adjustCurrentTime(i.getAttribute("time") / 1000);
+                        adjustCurrentTime(i.getAttribute("time"));
+                    }
+                }
+                element.querySelector(".mscz-track-select").onchange = () => {
+                    let track_id = parseInt(element.querySelector("select").value);
+                    for (let i = 0; i < audio_players.length; i++) {
+                        audio_players[i].pause();
+                    }
+                    if (track_id > 0) {
+                        audio_players[track_id].src = config.audio[track_id - 1].src;
+                    }
+                    for (let i of element.querySelectorAll(".mscz-measure-overlay")) {
+                        i.removeAttribute("time");
+                        i.classList.remove("mscz-measure-overlay-current");
+                    }
+                    for (let i of times[track_id]) {
+                        var measure_element = element.querySelector(`#mscz-measure-${currentId}-${i.id}`);
+                        if (measure_element && !measure_element.hasAttribute("time")) {
+                            measure_element.setAttribute("time", i.time);
+                        }
                     }
                 }
                 setInterval(() => {
-                    if (element.querySelector("select").value === "0") {
-                        setCurrentTime(midi_player.currentTime);
-                    }
+                    let track_id = parseInt(element.querySelector("select").value);
+                    setCurrentTime(audio_players[track_id].currentTime);
                 }, 100);
 
             }
-            midi_player.play();
+            let track_id = parseInt(element.querySelector("select").value);
+            audio_players[track_id].play();
         });
         element.querySelector(".mscz-pause-button").addEventListener("click", () => {
             if (!midi_player) { return };
-            midi_player.pause();
+            for (let i = 0; i < audio_players.length; i++) {
+                audio_players[i].pause();
+            }
         });
         element.querySelector(".mscz-back-button").addEventListener("click", () => {
             if (!midi_player) { return };
-            midi_player.currentTime = 0;
-            midi_player.pause();
+            for (let i = 0; i < audio_players.length; i++) {
+                audio_players[i].currentTime = 0;
+                audio_players[i].pause();
+            }
         });
     }
 };
